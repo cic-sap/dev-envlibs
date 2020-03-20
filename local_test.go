42e249d80f0221e302c70b653c1a44ef2aecf450
i521778 <jenkins.zhou@sap.com>
2020-03-20 11:55:03 +0800

    f

--- local_test.go
+++ local_test.go
@@ -1,9 +1,23 @@
 package envlibs
 
-import "testing"
+import (
+	"github.com/jarcoal/httpmock"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
 
 func TestFetchPkg(t *testing.T) {
-	p, tg, err := FetchPkg("eureka-jenkins-test", "gopm12", "0.0.1-ead8e9248eef394bdeaa2d6ca97ea2873c70bff7", "./output")
-	t.Logf("%+v, %+v, %+v", p, tg, err)
+	Convey("p1", t, func() {
+		httpmock.Activate()
+		defer httpmock.DeactivateAndReset()
+		// Exact URL match
+		httpmock.RegisterResponder("GET", "http://dev-info/env",
+			httpmock.NewStringResponder(200, `{"canary/*":"testing","canary/staging":"staging", "canary/hello":"hello"}`))
+		rs, err := GetValues("Eureka", "go-server-with-multi-values", "0.0.1-66ff51ee864ccfdbe411d092b362854c34e279fb", "./output", "canary", "hello7")
+		t.Logf("%+v, %+v",rs, err)
+		So(err, ShouldBeNil)
+		So(rs, ShouldHaveLength, 1)
+		So(rs[0], ShouldEqual, "values.testing.yaml")
+	})
 
 }
